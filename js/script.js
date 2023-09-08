0d59084c52720f7d60744507bda7c66d10fd966e
OluMel2 <129649026+OluMel2@users.noreply.github.com>
2023-09-08 14:52:17 +0000

    10章【ハンバーガーメニューを作ってみよう】

--- js/script.js
+++ js/script.js
@@ -5,6 +5,10 @@ $(function() {
     $(this).toggleClass('active');
     //.toggleClass()でclass属性を切り替える
     //.toggleClass()は、指定されているclass（ここではactive）をトグル処理するメソッド
+    //aタグのclassにはmenu-triggerが指定され、activeは指定されていません。
+    //このときに.toggleClass()を設定すると、アイコンがクリックされたときにactiveクラスが追加され、もう一度クリックすると削除されます。
+    //このように、.toggleClass()によって、class属性の追加と削除が交互に行われます。
+    //さらに、.menu-trigger.active span:nth-child(1)～(3)の各CSSによって、activeクラスになったときにアイコンが「×」になるように設定されています。
     $('#sp-menu').fadeToggle();
     //.fadeToggle()は、要素のフェードイン・フェードアウトを切り替えるメソッド
     //ここでは、#sp-menuが開いたときに、.fadeToggle()が処理されるように設定している。
